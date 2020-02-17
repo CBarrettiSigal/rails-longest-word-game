b834b78d53bdad5debc5e8068524afa6922d29d6
Christina Barreti Sigal <CBarrettiSigal@gmail.com>
2020-02-17 18:13:43 -0500

    Not done with the longest word challenge... but some of it is done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,31 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    alphabet = ('A'..'Z').to_a
+    10.times do
+      @letters << alphabet.sample
+    end
   end
 
   def score
+    @word = params[:word]
+    @message = "Placeholder"
+    @letters = params[:letters].split(' ')
+    params[:word].split("").each do |l|
+      if @letters.include?(l)
+      else
+        return @message = "Sorry, your word #{@word} is not inside #{@letters}"
+      end
+    end
   end
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    response = open(url)
+    json = JSON.parse(response.read)
+    json["found"]
+  end
+
+  if english_word?(@word)
+          return @message = "Your word is an English word"
 end
